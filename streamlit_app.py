b5e950e06ee5ea4ee01bb4a4a58c9044ae8e52df
jgitel <156343109+jgitel@users.noreply.github.com>
2024-11-04 12:17:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,12 @@ llm = OpenAI(openai_api_key=my_secret_key)
 ### Create a template to handle the case where the customer gives an unstructured review.
 airline_template = """You are an expert at customer service for an airline.
 From the following text, determine whether customer had a negative experience on their flight.
+If they did, determine if the cause was the airline's fault (e.g., lost luggage) or beyond the airline's control (e.g., a weather-related delay).
+Respond with two words. 
+1. Positive or negative. Pick one.
+2. Fault, not, or leave blank if 1. is positive. Pick one. 
 
-Do not respond with more than one word.
+Do not respond with more than two words.
 
 Text:
 {text}
@@ -36,22 +40,24 @@ Text:
 
 ### Create the decision-making chain
 
-experience_type_chain = (
+flight_experience_chain = (
     PromptTemplate.from_template(airline_template)
     | llm
     | StrOutputParser()
 )
 
 
-fault_chain = PromptTemplate.from_template(
-    """You are a customer service agent that is experienced with an airline's responsibilities. 
-Determine if the cause of the traveler's dissatisfaction is the airline's fault (e.g., lost luggage) or beyond the airline's control (e.g., a weather-related delay) from the following text.
-Do not respond with any reasoning. Just respond professionally as a customer service agent. Respond in first-person mode.
+negative_chain = PromptTemplate.from_template(
+    """You are an expert at customer service for an airline. 
+Given the text below, determine if the traveler who had a negative experience should receive compensation from the airline.
+Do not respond with any reasoning. Just respond professionally as a customer service agent. Respond in first-person mode. Leave text blank if traveler had positive experience.
 
 Your response should follow these guidelines:
-    1. Do not provide any reasoning behind the determination of fault. Just respond professionally as a customer service chat agent.
-    2. Address the customer directly
-
+    1. You will offer the traveler sympathies
+    2. If the airline is at fault for a negative experience, you will inform the user that customer service will contact them soon to resolve the issue or provide compensation
+    3. If the airline is not at fault for a negative experience, you will explain that the airline is not liable in such situations
+    4. Do not respond with any reasoning. Just respond professionally as a travel chat agent.
+    5. Address the customer directly  
 
 
 Text:
@@ -61,17 +67,17 @@ Text:
 ) | llm
 
 
-compensation_chain = PromptTemplate.from_template(
-    """You are a customer service agent for an airline.
-    Given the text below, determine if the traveler should receive compensation from the airline.
+positive_chain = PromptTemplate.from_template(
+    """You are an expert at customer service for an airline.
+    Given the text below, if the traveler had a positive experience, respond with the following.
+    Do not respond with any reasoning. Just respond professionally as a customer service agent. Respond in first-person mode. Leave text blank if traveler had negative experience.
 
     Your response should follow these guidelines:
-    1. You will offer the traveler sympathies 
-    2. If the airline is at fault for a negative experience, you will inform the user that customer service will contact them soon to resolve the issue or provide compensation
-    3. If the airline is not at fault for a negative experience, you will explain that the airline is not liable in such situations
-    4. If the user's experience is positive, thank them for their feedback and for choosing to fly with the airline. 
-    5. Do not respond with any reasoning. Just respond professionally as a travel chat agent.
-    6. Address the customer directly
+    1. You will thank the traveler for their feedback
+    2. You will thank the traveler for flying with our airline
+    3. You will wish the traveler well and offer them to reach out if they need anything
+    4. Do not respond with any reasoning. Just respond professionally as a travel chat agent.
+    5. Address the customer directly
 
 Text:
 {text}
@@ -84,19 +90,20 @@ from langchain_core.runnables import RunnableBranch
 
 ### Routing/Branching chain
 branch = RunnableBranch(
-    (lambda x: "text" in x["fault_type"].lower(), fault_chain),
-    compensation_chain,
+    (lambda x: "negative" in x["airline_template"].lower(), flight_experience_chain),
+    (lambda x: "positive" in x["airline_template"].lower(), flight_experience_chain),
+    negative_chain, positive_chain
 )
 
 ### Put all the chains together
-full_chain = {"fault_type": fault_chain, "text": lambda x: x["text"]} | branch
+full_chain = {"airline_template": flight_experience_chain, "text": lambda x: x["negative"], "text": lambda x: x["positive"], "text": negative_chain, "text": positive_chain} | branch
 
 
 
 import langchain
 langchain.debug = False
 
-full_chain.invoke({"text": prompt})
+response = full_chain.invoke({"text": prompt})
 
 ### Display
-st.write(full_chain)
+st.write(response)
